4ae3d8a597e864116cfa9ffeb093373c7b2ed2aa
Pablo Gallina <pablogallinag@gmail.com>
2021-10-23 12:34:18 -0600

    Ejemplo de scrollReveal

---
+++ public/js/scrollReveal.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.box',{ 
+    duration   : 600,
+    distance   : '20px',
+    easing     : 'ease-out',
+    origin     : 'bottom',
+    reset      : true,
+    scale      : 1,
+    //viewFactor : 0,
+    //afterReveal  : revealChildren,
+});
