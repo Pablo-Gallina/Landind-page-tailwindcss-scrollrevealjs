0c667c7ae94259bc1d6fd15e0880b9915c4153af
Pablo Gallina <pablogallinag@gmail.com>
2021-10-23 13:09:30 -0600

    Navbar

--- public/js/scrollReveal.js
+++ public/js/scrollReveal.js
@@ -1,4 +1,11 @@
-ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.navbar',{
+    duration   : 300,
+    distance   : '20px',
+    easing     : 'ease-in',
+    origin     : 'top',
+    reset      : true,
+    scale      : 1,
+});
 ScrollReveal().reveal('.box',{ 
     duration   : 600,
     distance   : '20px',
